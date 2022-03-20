b1937e45683a5ab510adc124b032a67009b32455
Дмитрий Мунгалов <mungalinio@gmail.com>
2022-03-20 21:00:56 +0300

    v-1 правки отступов
    Работа над адаптивом

--- src/app/start-page/start-page/start-page.component.ts
+++ src/app/start-page/start-page/start-page.component.ts
@@ -12,25 +12,25 @@ export class StartPageComponent implements OnInit {
 
   public sliders = [
     {
-      url: '../../../assets/img/slide-0-min.jpg',
+      url: '../../../assets/img/slide-1.jpg',
       heading: 'Бесплатная парковка',
       description: 'Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая ПДД, а также в аэропортах.',
       theme: 'green-theme'
     },
     {
-      url: '../../../assets/img/slide-1-min.jpg',
+      url: '../../../assets/img/slide-2.jpg',
       heading: 'Страховка',
       description: 'Полная страховка страховка автомобиля',
       theme: 'dark-blue-theme'
     },
     {
-      url: '../../../assets/img/slide-2-min.jpg',
+      url: '../../../assets/img/slide-3.jpg',
       heading: 'Бензин',
       description: 'Полный бак на любой заправке города за наш счёт',
       theme: 'fail-theme'
     },
     {
-      url: '../../../assets/img/slide-3-min.jpg',
+      url: '../../../assets/img/slide-4.jpg',
       heading: 'Обслуживание',
       description: 'Автомобиль проходит еженедельное ТО',
       theme: 'purple-theme'
