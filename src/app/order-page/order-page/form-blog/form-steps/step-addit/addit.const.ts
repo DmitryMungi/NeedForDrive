acc25728efe66f6ad26cd29f93659a4ca5f672ef
Дмитрий Мунгалов <mungalinio@gmail.com>
2022-04-14 14:21:55 +0300

    v-4 create checkbox

---
+++ src/app/order-page/order-page/form-blog/form-steps/step-addit/addit.const.ts
@@ -0,0 +1,1 @@
+export const CURENT_DATE = new Date();
--- src/app/order-page/order-page/form-blog/form-steps/step-addit/step-addit.component.ts
+++ src/app/order-page/order-page/form-blog/form-steps/step-addit/step-addit.component.ts
@@ -1,4 +1,14 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import {
+  ControlContainer,
+  FormGroup,
+  FormControl,
+  FormGroupDirective,
+  Validators,
+} from "@angular/forms";
+import { CURENT_DATE } from "./addit.const";
+
+import * as moment from "moment";
 
 export interface Icar {
   priceMax: number;
@@ -10,13 +20,31 @@ export interface Icar {
   colors: string[];
 }
 
+export interface Itarif {
+  name: string;
+}
+
 @Component({
   selector: "app-step-addit",
   templateUrl: "./step-addit.component.html",
   styleUrls: ["./step-addit.component.less"],
+  viewProviders: [
+    { provide: ControlContainer, useExisting: FormGroupDirective },
+  ],
 })
-export class StepAdditComponent {
-  constructor() {}
+export class StepAdditComponent implements OnInit {
+  public curentDate: string = moment(CURENT_DATE).format("yyyy-MM-DDThh:mm");
+
+  public formGroup = new FormGroup({
+    from: new FormControl(this.curentDate, Validators.required),
+    until: new FormControl("", Validators.required),
+  });
+  constructor(private orderForm: FormGroupDirective) {}
+
+  ngOnInit(): void {
+    this.orderForm.form.addControl("from", this.formGroup);
+    this.orderForm.form.addControl("until", this.formGroup);
+  }
 
   public car: Icar = {
     priceMax: 35000,
@@ -27,4 +55,21 @@ export class StepAdditComponent {
     categoryId: {},
     colors: ["Любой", "Красный", "Голубой"],
   };
+
+  public tarif: Itarif[] = [
+    {
+      name: "Поминутно, 7₽/мин",
+    },
+    {
+      name: "На сутки, 1999 ₽/сутки",
+    },
+  ];
+
+  valueChange(item: any) {
+    console.log(item);
+  }
+
+  onDeleteValue(item: any) {
+    item.value = "";
+  }
 }
